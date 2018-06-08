4fe0bb65ae45483166a800c7820d47775e8cb73d
Fagun Rain <fagunrain@Faguns-MacBook-Pro.local>
2018-06-08 13:25:05 +0600

    Add splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
+import { SplashscreenPage } from './../pages/splashscreen/splashscreen';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = SplashscreenPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
     });
   }
 }
---
+++ src/pages/splashscreen/splashscreen.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * Generated class for the SplashscreenPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-splashscreen',
+  templateUrl: 'splashscreen.html',
+})
+export class SplashscreenPage {
+
+  constructor() {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SplashscreenPage');
+  }
+
+}
